215913509053a6eb643c52d8e4af8cb04b139ef2
Klaus Wagner <k.wagner@student.tugraz.at>
2014-04-14 14:53:55 +0200

    [KW]{other} - Added a test project to the git repository.

---
+++ Emporium_test/gen/com/mobileapplications/emporium/test/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.mobileapplications.emporium.test;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
