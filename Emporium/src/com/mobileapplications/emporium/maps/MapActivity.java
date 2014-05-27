b5825ac36fe6f9589c1c64415160dff3d1c8f164
Mustafa Mahmoud <m.mahmoud@student.tugraz.at>
2014-05-27 11:17:04 +0200

    [MM,PS]{story} - Added new GPSTracking Class and fixed some issues

--- Emporium/src/com/mobileapplications/emporium/maps/MapActivity.java
+++ Emporium/src/com/mobileapplications/emporium/maps/MapActivity.java
@@ -99,65 +99,6 @@ public class MapActivity extends Activity
 	        super.onDestroy();
 	    }
 	    
-	    public Location getLocation() {
-	        try {
-	            LocationManager locationManager = (LocationManager) mContext.getSystemService(LOCATION_SERVICE);
-
-	            // getting GPS status
-	            boolean isGPSEnabled = locationManager
-	                    .isProviderEnabled(LocationManager.GPS_PROVIDER);
-
-	            // getting network status
-	            boolean isNetworkEnabled = locationManager
-	                    .isProviderEnabled(LocationManager.NETWORK_PROVIDER);
-
-	            if (!isGPSEnabled && !isNetworkEnabled) {
-	                // no network provider is enabled
-	            } else {
-	                this.canGetLocation = true;
-	                if (isNetworkEnabled) {
-	                    locationManager.requestLocationUpdates(
-	                            LocationManager.NETWORK_PROVIDER,
-	                            MIN_TIME_BW_UPDATES,
-	                            MIN_DISTANCE_CHANGE_FOR_UPDATES, this);
-	                    Log.d("Network", "Network Enabled");
-	                    if (locationManager != null) {
-	                        location = locationManager
-	                                .getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
-	                        if (location != null) {
-	                            latitude = location.getLatitude();
-	                            longitude = location.getLongitude();
-	                        }
-	                    }
-	                }
-	                // if GPS Enabled get lat/long using GPS Services
-	                if (isGPSEnabled) {
-	                    if (location == null) {
-	                        locationManager.requestLocationUpdates(
-	                                LocationManager.GPS_PROVIDER,
-	                                MIN_TIME_BW_UPDATES,
-	                                MIN_DISTANCE_CHANGE_FOR_UPDATES, this);
-	                        Log.d("GPS", "GPS Enabled");
-	                        if (locationManager != null) {
-	                            location = locationManager
-	                                    .getLastKnownLocation(LocationManager.GPS_PROVIDER);
-	                            if (location != null) {
-	                                latitude = location.getLatitude();
-	                                longitude = location.getLongitude();
-	                            }
-	                        }
-	                    }
-	                }
-	            }
-
-	        } catch (Exception e) {
-	            e.printStackTrace();
-	        }
-
-	        return location;
-	    }
-	    
-
 }
 
 
